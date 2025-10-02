4b3fdeb1f4af33190fa5506334bd4651f80b731e
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-03 00:00:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import smtplib, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+sender   = os.getenv("MAIL_USER")
+password = os.getenv("MAIL_PASS")
+receiver = os.getenv("MAIL_TO")
+file_path = "file.txt"
+
+msg = MIMEMultipart()
+msg["From"]    = sender
+msg["To"]      = receiver
+msg["Subject"] = os.getenv("MAIL_SUBJECT")
+
+with open(file_path, "rb") as f:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(f.read())
+encoders.encode_base64(part)
+part.add_header("Content-Disposition", f'attachment; filename="{file_path}"')
+msg.attach(part)
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    server.login(sender, password)
+    server.sendmail(sender, receiver, msg.as_string())
